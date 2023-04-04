99a9ddbb80ab861f5e29b515b837f6857f4adba7
kocakaysegul <aysegulkocak112@gmail.com>
2023-04-04 13:09:20 +0300

    Project Setup and DataModel

---
+++ RecipeApp/Model/FoodCategory.swift
@@ -0,0 +1,14 @@
+//
+//  FoodCategory.swift
+//  RecipeApp
+//
+//  Created by Ayşegül Koçak on 4.04.2023.
+//
+
+import Foundation
+
+struct FoodCategory {
+    
+    let title : String
+    let imageName : String
+}
---
+++ RecipeApp/Model/Recipe.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe.swift
+//  RecipeApp
+//
+//  Created by Ayşegül Koçak on 4.04.2023.
+//
+
+import Foundation
+
+struct Recipe {
+    
+    let title : String
+    let instructions : String
+    let imageName : String
+}
