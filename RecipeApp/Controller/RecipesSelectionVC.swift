eba612c39d57d14798d8d10b846cf9cb4422296f
kocakaysegul <aysegulkocak112@gmail.com>
2023-04-04 17:13:00 +0300

    Passing data in PrepareforSegue

---
+++ RecipeApp/Controller/RecipesSelectionVC.swift
@@ -0,0 +1,33 @@
+//
+//  RecipesSelectionVC.swift
+//  RecipeApp
+//
+//  Created by Ayşegül Koçak on 4.04.2023.
+//
+
+import UIKit
+
+class RecipesSelectionVC: UIViewController {
+
+    
+    var selectedCategory : String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print(selectedCategory)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
