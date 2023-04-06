715e5e056ce56584ac80eee74b84b3b33445bf78
kocakaysegul <aysegulkocak112@gmail.com>
2023-04-06 08:42:01 +0300

    Recipe Detail VC designed

---
+++ RecipeApp/Controller/RecipeDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  RecipeDetailVC.swift
+//  RecipeApp
+//
+//  Created by Ayşegül Koçak on 5.04.2023.
+//
+
+import UIKit
+
+class RecipeDetailVC: UIViewController {
+    
+    @IBOutlet weak var recipeImage: UIImageView!
+    
+    @IBOutlet weak var recipeTitleLabel: UILabel!
+    
+    @IBOutlet weak var recipeInstructionsLabel: UILabel!
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
